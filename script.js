a8a799406e01d6c2d32103e1b13b9133b9d1ff9d
bds8 <bds8@hi.is>
2017-11-16 13:00:08 +0000

    Parteyyy

---
+++ script.js
@@ -0,0 +1,5 @@
+data = JSON.parse(response);
+data.videos // til að ná í videos
+data.categories[0] // færðu fyrsta stakið í categories fylkinu....
+data.categories[0].title // þá færðu titillinn nýleg myndbönd
+data.categories[0].videos // þá færðu videos fylkið [1,2,3]
