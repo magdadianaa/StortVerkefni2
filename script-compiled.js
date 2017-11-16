cf38774da651e88a4e8c9c22ec1a411d1fea3255
Ásta Lára Magnúsdóttir <astalara@askja-dw316.rhi.hi.is>
2017-11-16 13:43:10 +0000

    uppkast af html og grid.css

---
+++ script-compiled.js
@@ -0,0 +1,9 @@
+"use strict";
+
+data = JSON.parse(response);
+data.videos; // til að ná í videos
+data.categories[0]; // færðu fyrsta stakið í categories fylkinu....
+data.categories[0].title; // þá færðu titillinn nýleg myndbönd
+data.categories[0].videos; // þá færðu videos fylkið [1,2,3]
+
+//# sourceMappingURL=script-compiled.js.map
